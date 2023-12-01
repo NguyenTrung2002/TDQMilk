6329d6774a6ea30174b2a3bc8b65e170e88d54f3
NguyenTrung2002 <NguyenTrung2002>
2023-12-01 11:25:03 +0700

    trung

--- pages/menu.php
+++ pages/menu.php
@@ -6,13 +6,15 @@ $query_danhmuc = mysqli_query($conn, $sql_danhmuc);
     <div class="headline">
         <h3>Danh mục sản phẩm</h3>
     </div>
-    <ul id="main-menu">
+    <div class="pagination">
+        <a href="#">&laquo;</a>
         <?php
         while ($row_danhmuc = mysqli_fetch_array($query_danhmuc)) {
         ?>
-            <li><a href="index.php?quanly=danhmucsanpham&id=<?php echo $row_danhmuc['id_danhmuc'] ?>"><?php echo $row_danhmuc['ten_danhmuc'] ?></a></li>
+            <a href="index.php?quanly=danhmucsanpham&id=<?php echo $row_danhmuc['id_danhmuc'] ?>"><?php echo $row_danhmuc['ten_danhmuc'] ?></a>
         <?php
         }
         ?>
-    </ul>
-</div>
+        <a href="#">&raquo;</a>
+        </ul>
+    </div>
